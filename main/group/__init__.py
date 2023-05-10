cede885460e3ca6d0192fb9261d38618eb9bed84
김민관 <alsrhks2508@naver.com>
2023-05-11 03:41:18 +0900

    그룹 패키지 초기 작성

---
+++ main/group/__init__.py
@@ -0,0 +1,3 @@
+from group.groupClass import *
+from group.management import *
+from group.constant import *
---
+++ main/group/management.py
@@ -0,0 +1,10 @@
+class Group_Manger:
+    def __init__(self, db):
+        self.db = db
+        
+
+
+class Make_Group:
+    def __init__():
+        pass
+
