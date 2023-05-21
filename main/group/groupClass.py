9f672029f4518c57d7429daf8baac7dbc21159e5
김민관 <alsrhks2508@naver.com>
2023-05-21 20:20:38 +0900

    접근자 추가

--- main/group/groupClass.py
+++ main/group/groupClass.py
@@ -3,11 +3,13 @@ from group.constant import GROUP_COL, GROUP_DATA
 class Group:
     def __init__(self, db, index):
         data = db.get_data_index(GROUP_COL, index)
+        self._id = data['_id']
         self._group_member = []
         self.index = data[GROUP_DATA[0]]
         self._group_name = data[GROUP_DATA[1]]
         self._group_purpose = data[GROUP_DATA[2]]
         self._size = data[GROUP_DATA[3]]
+        self._group_issue = data[GROUP_DATA[4]]
 
         for i in range (self._size):
             if i == 0:
@@ -21,7 +23,20 @@ class Group:
         print("group group purpose : ", self._group_purpose)
         print("group group size : ", self._size)
         print("group member : ", self._group_member)
+        print("group issue : ", self._group_issue)
 
+    def set_group_issue(self, txt):
+        self._group_issue = txt
+        return self._id
+    
+    def set_group_member(self, key, name):
+        self._group_member[key] = name
+
+    def get_group_id(self):
+        return self._id
+
+    def get_issue(self):
+        return self._group_issue
 
     def get_group_member(self):
         return self._group_member
