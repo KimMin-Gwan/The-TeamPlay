98574b9d0c2fbf8b8b8577c9db22ed346eab12d3
김민관 <alsrhks2508@naver.com>
2023-05-21 16:03:35 +0900

    Update guiTest.py

--- develop/guiTest.py
+++ develop/guiTest.py
@@ -1,49 +1,37 @@
 import tkinter as tk
+from tkinter.font import Font
 
-class LoginWindow(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.master = master
+root = tk.Tk()
 
-        self.username_entry = tk.Entry(self)
-        self.password_entry = tk.Entry(self, show="*")
-        self.login_button = tk.Button(self, text="로그인", command=self.login)
+canvas_width = 300
+canvas_height = 200
 
-        self.username_entry.pack()
-        self.password_entry.pack()
-        self.login_button.pack()
+# Canvas 생성
+canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
+canvas.pack()
 
-    def login(self):
-        # 로그인 로직을 처리하는 코드
-        # 예를 들면, 사용자가 입력한 정보를 확인하고 인증하는 과정 등
+text = "This is a long text that needs to be wrapped to the next line if it exceeds the wrap length."
+wraplength = 250  # 한 줄의 최대 너비
 
-        # 로그인 성공 시 다른 클래스로 이동
-        self.master.switch_to_other_window()
+# 텍스트 출력
+lines = []
+current_line = ""
+words = text.split()
 
-class OtherWindow(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.master = master
+font = Font(family="Arial", size=20)  # 폰트 설정
+for word in words:
+    if font.measure(current_line + " " + word) <= wraplength:
+        current_line += " " + word
+    else:
+        lines.append(current_line)
+        current_line = word
 
-        self.label = tk.Label(self, text="다른 창")
-        self.label.pack()
+lines.append(current_line)
 
-class MainWindow(tk.Tk):
-    def __init__(self):
-        super().__init__()
+y = 50  # 시작 y 좌표
+for line in lines:
+    canvas.create_text(canvas_width // 2, y, text=line, font=font, anchor=tk.CENTER)
+    y += 30  # 줄 간격 조정
 
-        self.title("메인 창")
-        self.geometry("300x200")
+root.mainloop()
 
-        self.login_window = LoginWindow(self)
-        self.other_window = OtherWindow(self)
-
-        self.login_window.pack()
-
-    def switch_to_other_window(self):
-        self.login_window.pack_forget()
-        self.other_window.pack()
-
-if __name__ == "__main__":
-    app = MainWindow()
-    app.mainloop()
