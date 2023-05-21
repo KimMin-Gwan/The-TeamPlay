d08c1fe69c6a1a775a2d6f39806985b544b2837e
김민관 <alsrhks2508@naver.com>
2023-05-21 20:21:31 +0900

    교수님 클래스 작성

--- main/others/otherClass.py
+++ main/others/otherClass.py
@@ -1,12 +1,31 @@
+from others.constant import PROF_COL, PROF_DATA
+
 class Rental_Post:
     def __init__():
         pass
 
-class Colabo_Post:
-    def __init__():
-        pass
+class Professor():
+    def __init__(self, pf_data):
+        self._data = {}
+        self._data[PROF_DATA[0]] = pf_data[PROF_DATA[0]]  # name
+        self._data[PROF_DATA[1]] = pf_data[PROF_DATA[1]]  # email
+        self._data[PROF_DATA[2]] = pf_data[PROF_DATA[2]]  # major
+        self._data[PROF_DATA[3]] = pf_data[PROF_DATA[3]]  # office
+        self._data[PROF_DATA[4]] = pf_data[PROF_DATA[4]]  # assistant_tel
+        self._data[PROF_DATA[5]] = pf_data[PROF_DATA[5]]  # office_tel
+
+    def __call__(self):
+        print("Professor information")
+        print("office: ", self._data[PROF_DATA[0]])
+        print("assistant_tel: ", self._data[PROF_DATA[1]])
+        print("office_tel: ", self._data[PROF_DATA[2]])
+
+    def get_data(self):
+        return self._data
+    
+    def get_name_major(self):
+        nm_mj = self._data[PROF_DATA[0]] + ' , ' + self._data[PROF_DATA[2]]
+        return nm_mj
+        
 
 
-class Professor:
-    def __init__():
-        pass
