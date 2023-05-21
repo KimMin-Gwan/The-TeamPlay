422f3f735e19d51a66f044ab784c4e6a93a0760c
김민관 <alsrhks2508@naver.com>
2023-05-21 20:21:39 +0900

    교수님 상수

---
+++ main/others/constant.py
@@ -0,0 +1,8 @@
+PROF_COL = "prof_data"
+PROF_DATA = ["name",
+             "email",
+             "major",
+             "office", 
+             "assistant_tel",
+             "office_tel"
+             ]
