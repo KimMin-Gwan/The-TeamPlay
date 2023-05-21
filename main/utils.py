ca71c29ed2348ee085abd147bc5d904d4722ee98
김민관 <alsrhks2508@naver.com>
2023-05-21 20:22:37 +0900

    디비 접근자, 삭제자 추가

--- main/utils.py
+++ main/utils.py
@@ -18,10 +18,24 @@ class Databass():
         print('input data in db(id) : ', dct_id)
         return
     
-    def get_data_list(self, collection, type):
+    def get_data_list(self, collection, type = None):
         target_col = self.db[collection]
-        cursor = target_col.find({'type': type})
-        result = list(cursor)
+        try:
+            cursor = target_col.find({'type': type})
+            result = list(cursor)
+        except:
+            cursor = target_col.find()
+            result = list(cursor)
+        return result 
+
+    def get_data_list2(self, collection, index):
+        target_col = self.db[collection]
+        try:
+            cursor = target_col.find({'index': index})
+            result = list(cursor)
+        except:
+            cursor = target_col.find()
+            result = list(cursor)
         return result 
     
     def get_data_id(self, collection, ID):
@@ -48,6 +62,14 @@ class Databass():
             return data
         except:
             return None
+
+    def get_data_nick_name(self, collection, nn):
+        try:
+            target_col = self.db[collection]
+            data = target_col.find_one({"nick_name": nn})
+            return data
+        except:
+            return None
         
     def update_data(self, collection, id, key, data):
         try:
@@ -58,6 +80,23 @@ class Databass():
             print("Error :", str(e))
             return 
         
+    def get_data_name(self, collection, name):
+        try:
+            target_col = self.db[collection]
+            data = target_col.find_one({"name": name})
+            return data
+        except:
+            return -1
+        
+    def update_join_data(self, collection, id, key, data):
+        try:
+            print("target :", key)
+            target_col = self.db[collection]
+            target_col.update_one({"_id" : id}, {"$set" : { key : data }})
+        except Exception as e:
+            print("Error :", str(e))
+            return 
+
     def get_size_col(self, collection):
         count = 0
         try:
@@ -66,6 +105,15 @@ class Databass():
         except:
             count = -1
         return count
+    
+    def delete_data(self, collection, key, value):
+        try:
+            target_col = self.db[collection]
+            filter = {key : value}
+            target_col.delete_one(filter)
+        except Exception as e:
+            print("Error :", str(e))
+            return 
         
 
 """
